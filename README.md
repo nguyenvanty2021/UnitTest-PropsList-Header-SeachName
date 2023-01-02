b15696ef0d553dd91e461e9f31b5b180cfcf79f2
nguyenvanty2021 <nguyenvanty2021>
2023-01-02 19:15:53 +0700

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# **Episode 2** - When & How to TDD
+
+### This is the second episode of the 🎬[NextJS & Tests video series](https://www.youtube.com/playlist?list=PLCBa_75YlKx6cQc0JNZz-L1ZGFb-pOIyL) . Here I show how to write tests , mocking props , mimicing user events & many more . I will create the entire homepage & will write 5 tests while creating a accessible search box , render all products , render all categories & their products etc...
+
+&nbsp;  
+ &nbsp;  
+ &nbsp;
+
+## **Watch The Full Video Here** : [![Episode 2](https://i.ytimg.com/vi/ru-zQQUr744/maxresdefault.jpg)](https://youtu.be/ru-zQQUr744)
+
+&nbsp;
+
+&nbsp;
+&nbsp;
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
---
+++ pages/index.js
@@ -0,0 +1,84 @@
+import commerce from "../lib/commerce";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+export async function getStaticProps(context) {
+  const { data: products } = await commerce.products.list();
+  const { data: categories } = await commerce.categories.list();
+
+  return {
+    props: { products, categories },
+    revalidate: 30,
+  };
+}
+
+export default function Home({ products, categories }) {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Online Store</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <input
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+          role="searchbox"
+          type="text"
+          title="Search"
+          placeholder="Search products"
+        />
+
+        {searchTerm.length > 0 ? (
+          <>
+            <h2 id="search-results-heading">Search results</h2>
+            <ul aria-labelledby="search-results-heading">
+              {products
+                .filter((product) =>
+                  product.name.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((product) => {
+                  return <li key={product.id}>{product.name}</li>;
+                })}
+            </ul>
+          </>
+        ) : (
+          <>
+            <ul aria-label="Categories">
+              {categories.map((category) => {
+                return (
+                  <li aria-label="category" key={category.id}>
+                    <h2 id={`category-${category.name}`}>{category.name}</h2>
+                    <ul aria-labelledby={`category-${category.name}`}>
+                      {products
+                        .filter((product) =>
+                          product.categories.find((c) => c.id === category.id)
+                        )
+                        .map((product) => {
+                          return <li key={product.id}>{product.name}</li>;
+                        })}
+                    </ul>
+                  </li>
+                );
+              })}
+            </ul>
+
+            <h2 id="all-products-heading">All Products</h2>
+            <ul aria-labelledby="all-products-heading">
+              {products.map((product) => {
+                return <li key={product.id}>{product.name}</li>;
+              })}
+            </ul>
+          </>
+        )}
+      </main>
+
+      <footer className={styles.footer}>Made by Abhik B</footer>
+    </div>
+  );
+}
